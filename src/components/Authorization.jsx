ea871f36f94f3c7ae04435fdc533b04afab0160f
Ningaro <23503775+Ningaro@users.noreply.github.com>
2022-01-31 15:54:30 +0300

    ⚡️Image in Button change to IconButton elem

--- src/components/Authorization.jsx
+++ src/components/Authorization.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Button, HStack, Image, Text, useDisclosure } from '@chakra-ui/react';
+import {
+    Link,
+    IconButton,
+    useDisclosure
+} from '@chakra-ui/react';
 import AuthFormModal from './AuthFormModal';
-import singinIcon from '../img/Icons/ExitIcon.png'
 
 function Authorization(props) {
     const { isOpen, onOpen, onClose } = useDisclosure()
@@ -12,31 +15,22 @@ function Authorization(props) {
         />
     )
     return (
-        <HStack spacing={{ base: '1', md: '5' }} display="flex">
-            <Button onClick={onOpen} variant='link'>
-
-                {/* для экранов шире чем 992px */}
-                <Text 
-                    fontSize='2xl' 
-                    color='white' 
-                    display={{ base: "none", lg: "block" }}
-                >
-                    Войти/Регистрация
-                </Text>
-
-                {/* для экранов уже чем 992px */}
-                <Image
-                    src={singinIcon}
-                    display={{ base: "block", lg: "none" }}
-                    transition="color 0.2s"
-                    minW='8'
-                    minH='8'
-                    _hover={{ color: "gray.600" }}
-                />
-            </Button>
+        <>
+            <Link
+                fontSize='2xl'
+                color='white's
+                onClick={onOpen}
+                display={{ base: "none", lg: "inherit" }}
+            >
+                Войти/Регистрация
+            </Link>
+            <IconButton
+                mx={1}
+                bg="transparent"
+                display={{ base: "inherit", lg: "none" }}
+            />
             {modal}
-
-        </HStack>
+        </>
     );
 }
 
