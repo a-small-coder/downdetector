ac40af2c8d8eba00bc4070cafba7c15770916793
Ningaro <23503775+Ningaro@users.noreply.github.com>
2022-01-31 15:52:15 +0300

    ⚡️Change svg Log and Reg icons to react-icons

--- src/components/AuthFormModal.jsx
+++ src/components/AuthFormModal.jsx
@@ -3,14 +3,11 @@ import React from 'react';
 import LoginForm from './LoginForm';
 import RadioBox from './RadioBox';
 import RegisterForm from './RegisterForm';
-
-const LoginIcon = (props) => (
-    <svg style={{marginLeft: '12px'}} width="31" height="20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M11.81 5.44a1.5 1.5 0 0 1 0 2.12l-6 6a1.5 1.5 0 0 1-2.12 0l-3-3a1.5 1.5 0 0 1 2.12-2.12l1.94 1.939 4.94-4.94a1.5 1.5 0 0 1 2.12 0Z" fill="#fff" fillOpacity=".92" /><path d="M25.25 5c0-2.763-2.238-5-5-5s-5 2.237-5 5 2.238 5 5 5 5-2.237 5-5Zm-5 7c3.605 0 10 1.23 10 5.538v.87c0 .88-.713 1.592-1.592 1.592H11.842c-.88 0-1.592-.713-1.592-1.592v-.87c0-4.307 6.395-5.538 10-5.538Z" fill="#fff" fillOpacity=".92" /></svg>
-)
-
-const RegIcon = (props) => (
-    <svg style={{marginLeft: '12px'}} width="31" height="20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.25 5c0-2.763 2.237-5 5-5 2.762 0 5 2.237 5 5s-2.238 5-5 5c-2.763 0-5-2.237-5-5Zm5 7c-3.605 0-10 1.23-10 5.538v.87C.25 19.288.963 20 1.842 20h16.816c.88 0 1.592-.713 1.592-1.592v-.87c0-4.307-6.395-5.538-10-5.538Zm14.5-8a1.5 1.5 0 0 0-1.5 1.5V8h-2.5a1.5 1.5 0 0 0 0 3h2.5v2.5a1.5 1.5 0 0 0 3 0V11h2.5a1.5 1.5 0 0 0 0-3h-2.5V5.5a1.5 1.5 0 0 0-1.5-1.5Z" fill="#fff" fillOpacity=".92" /></svg>
-)
+// Icons from library
+import {
+    RiUserAddFill,
+    RiUserFollowFill,
+} from 'react-icons/ri'
 
 function AuthFormModal(props) {
     const [isLoginForm, setIsLoginForm] = React.useState(true)
@@ -76,8 +73,10 @@ function AuthFormModal(props) {
                                 else{
                                     return (
                                         <RadioBox key={value} {...radio}>
-                                            {value === 'reg' ? 'Регистрация' : 'Вход' } 
-                                            {value === 'reg' ? <RegIcon/> :  <LoginIcon/>} 
+                                            {value === 'reg' ? 'Регистрация' : 'Вход'}
+                                            <chakra.span ml={1}>
+                                                {value === 'reg' ? <RiUserAddFill /> : <RiUserFollowFill />}
+                                            </chakra.span>
                                         </RadioBox>
                                     )
                                 }
