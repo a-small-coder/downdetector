a771398bc8b8f1204b5367e604b3aa7532ab9e4f
oleg <hulktv222@mail.ru>
2022-02-09 15:59:15 +0300

    fix icon border

--- src/components/Authorization.jsx
+++ src/components/Authorization.jsx
@@ -20,29 +20,12 @@ function Authorization(props) {
     )
     return (
         <>
-        {props.isAuth ? 
-            (<IconButton
+        <IconButton
                 mx={1}
-                icon={<FiUserCheck />}
+                icon={props.isAuth ? <FiUserCheck /> : <FiUserPlus />}
                 fontSize={{base: '24px', lg: '32px'}}
                 pr='4px'
-                bg="transparent"
-                color='white'
-                onClick={onOpen}
-                _active={{
-                    bg: 'inherit',
-                }}
-                _hover={{
-                    bg: 'inherit',
-                    border: '2px solid white'
-                }}
-            /> )
-            :
-            (<IconButton
-                mx={1}
-                fontSize={{base: '24px', lg: '32px'}}
-                pr='4px'
-                icon={<FiUserPlus />}
+                mr='2'
                 bg="transparent"
                 color='white'
                 onClick={onOpen}
@@ -53,8 +36,7 @@ function Authorization(props) {
                     bg: 'inherit',
                     transform: 'scale(1.2)'
                 }}
-            />)
-        }
+            /> 
             {modal}
 
         </>
