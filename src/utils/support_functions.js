7c63875f231375a5d8d96e09cd08fb209003faa8
Ningaro <23503775+Ningaro@users.noreply.github.com>
2022-04-08 02:30:24 +0300

    ⚡️Add helper function random

--- src/utils/support_functions.js
+++ src/utils/support_functions.js
@@ -1,38 +1,50 @@
 // user token
 
 export function getStorageUserToken() {
-    let user = JSON.parse(localStorage.getItem('user'));
-  
-    if (user && user.token) {
-        return user.token;
-    } else {
-        return null;
-    }
-  }
-  
-  export function setStorageUser(token) {
-    let user = {
-      token: token
-    }
-    localStorage.setItem('user', JSON.stringify(user));
-  }
-  
-  export function removeStorageUser(){
-    localStorage.removeItem('user')
-  }
+  let user = JSON.parse(localStorage.getItem('user'))
 
-  // избранные компании
-
-  export function getStorageFavorities(){
-    let favoriteServices = JSON.parse(localStorage.getItem('favoriteServices'));
-  
-    return favoriteServices
+  if (user && user.token) {
+    return user.token
+  } else {
+    return null
   }
+}
 
-  export function setStorageFavorities(services){
-      localStorage.setItem('favoriteServices', JSON.stringify(services))
+export function setStorageUser(token) {
+  let user = {
+    token: token,
   }
+  localStorage.setItem('user', JSON.stringify(user))
+}
 
-  export function removeStorageFavorities(){
-    localStorage.removeItem('favoriteServices')
-  }
+export function removeStorageUser() {
+  localStorage.removeItem('user')
+}
+
+// избранные компании
+
+export function getStorageFavorities() {
+  let favoriteServices = JSON.parse(localStorage.getItem('favoriteServices'))
+
+  return favoriteServices
+}
+
+export function setStorageFavorities(services) {
+  localStorage.setItem('favoriteServices', JSON.stringify(services))
+}
+
+export function removeStorageFavorities() {
+  localStorage.removeItem('favoriteServices')
+}
+
+/**
+ * Randomize function
+ * @param min minimal value
+ * @param max max value
+ * @returns Random value from min to max (included)
+ */
+export function random(min, max) {
+  const minRounded = Math.ceil(min)
+  const maxRounded = Math.floor(max)
+  return Math.floor(Math.random() * (maxRounded - minRounded + 1)) + minRounded
+}
